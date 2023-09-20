699c54f385deaeeb2b0945d278df6544a3826acb
Michael Felix <mikemh@uri.edu>
2023-09-20 12:10:41 -0400

    a

--- hw/hw1/csc406-hw1/csc406-hw1/dolphin.cpp
+++ hw/hw1/csc406-hw1/csc406-hw1/dolphin.cpp
@@ -6,6 +6,51 @@
 //
 
 #include "dolphin.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+void loadShape(float** shapePoints, const char* filename) {
+    // Open the file for reading
+    std::ifstream inputFile(filename);
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    int pointIndex = 0;
+
+    // Read and process each line in the file
+    while (std::getline(inputFile, line)) {
+        std::istringstream iss(line);
+        std::string token;
+
+        while (std::getline(iss, token, ',')) {
+            if (pointIndex >= 2) {
+                std::cerr << "Error: Too many values in the file." << std::endl;
+                inputFile.close();
+                return;
+            }
+
+            try {
+                float value = std::stof(token);
+                shapePoints[pointIndex][pointIndex] = value;
+                pointIndex++;
+            } catch (const std::invalid_argument& e) {
+                std::cerr << "Error: Invalid float value in the file." << std::endl;
+                inputFile.close();
+                return;
+            }
+        }
+    }
+
+    // Close the file
+    inputFile.close();
+}
+
 
 void initShape(float** shapePoints) {
     shapePoints[0] = new float[2];
