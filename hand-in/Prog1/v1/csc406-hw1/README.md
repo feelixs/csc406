9cd410ce9e93fec1ed89b9dfe0eba99c1bbb907e
Michael Felix <mikemh@uri.edu>
2023-09-27 13:45:30 -0400

    hand in

---
+++ hand-in/Prog1/v1/csc406-hw1/README.md
@@ -0,0 +1,7 @@
+This is the first version of my project 1.
+
+I implemented a loadShape() function.
+
+To get the xcode project to work, you must first change the filepath of 
+main.cpp line 215 to reference the filepath of the 'shapeCoords.txt' file 
+in this folder
---
+++ hand-in/Prog1/v1/csc406-hw1/dolphin.hpp
@@ -0,0 +1,37 @@
+//
+//  dolphin.hpp
+//  csc406-hw1
+//
+//  Created by Michael Felix on 9/20/23.
+//
+
+#ifndef dolphin_hpp
+#define dolphin_hpp
+
+#include <stdio.h>
+
+void loadShape(const char* filename); // returns the amount of vertices in the loaded file
+
+class Dolphin
+{
+    friend int loadShape();
+    
+    private:
+        float centerX_, centerY_, scaleX_, scaleY_, angle_;
+        float red_, green_, blue_;
+
+    public:
+        Dolphin(float centerX, float centerY, float angle, float scaleX, float scaleY, float r, float g, float b);
+        ~Dolphin();
+        
+        void draw() const;
+
+        //disabled constructors & operators
+        Dolphin() = delete;
+        Dolphin(const Dolphin& obj) = delete;    // copy
+        Dolphin(Dolphin&& obj) = delete;        // move
+        Dolphin& operator = (const Dolphin& obj) = delete;    // copy operator
+        Dolphin& operator = (Dolphin&& obj) = delete;        // move operator
+};
+
+#endif /* dolphin_hpp */
