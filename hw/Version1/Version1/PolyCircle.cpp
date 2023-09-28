d0d3b3bb1bd0f6523a65d38125c52db5ee50ee2b
Michael Felix <mikemh@uri.edu>
2023-09-28 12:47:32 -0400

    Update PolyCircle.cpp

--- hw/Version1/Version1/PolyCircle.cpp
+++ hw/Version1/Version1/PolyCircle.cpp
@@ -30,10 +30,10 @@ PolyCircle::PolyCircle(float centerX, float centerY, float angle, float scaleX, 
 
 
 PolyCircle::~PolyCircle() {
-    PolyShape::~PolyShape();
     for (int i = 0; i < _numCirPoints; i++) {
         delete[] _circlePoints[i];
     }
     delete[] _circlePoints;
     std::cout << "Cicle at " << centerX_ << ", " << centerY_ << " was deleted" << std::endl;
+    PolyShape::~PolyShape();
 }
