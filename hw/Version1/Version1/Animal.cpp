dc75adf5df9900eef51b49c04cb971a456e82298
Michael Felix <mikemh@uri.edu>
2023-09-29 22:05:22 -0400

    Update Animal.cpp

--- hw/Version1/Version1/Animal.cpp
+++ hw/Version1/Version1/Animal.cpp
@@ -38,24 +38,19 @@ Animal::Animal(float centerX, float centerY, float angle, float scaleX, float sc
 {
     // this is where we programatically draw the animal
     
-    // two half-circles at different angles (face)
     
+    // wings
     _myShapes.push_back(std::make_shared<PolyEarRight>(animalShapesDir + "/rightEar.txt", centerX_+(scaleX_/10.f), centerY_+(scaleY_/1.6), angle_-10, scaleX_, scaleY_, red_, green_, blue_));
     _myShapes.push_back(std::make_shared<PolyEarLeft>(animalShapesDir + "/leftEar.txt", centerX_-(scaleX_/10.f), centerY_+(scaleY_/1.6), angle_+10, scaleX_, scaleY_, red_, green_, blue_));
-    
+    // two half-circles at different angles (face/body)
     _myShapes.push_back(std::make_shared<PolyHalfCircle>(animalShapesDir + "/halfCircle.txt", centerX_, centerY_, angle_+4.f, scaleX_, scaleY_, red_, green_, blue_));
     _myShapes.push_back(std::make_shared<PolyHalfCircle>(animalShapesDir + "/halfCircle.txt", centerX_, centerY_, angle_+14.f, scaleX_, scaleY_, red_, green_, blue_));
-    
-//    _myShapes.push_back(std::make_shared<PolyCircle>(centerX_, centerY_, angle_, scaleX_, scaleY_, red_, green_, blue_));
-
-    
     // left eye
     _myShapes.push_back(std::make_shared<PolyCircle>(centerX_-(scaleX_/2.4), centerY_+(scaleY_/1.9), angle_, scaleX_/8.f, scaleY_/8.f, 0.f, 0.f, 0.f));
     _myShapes.push_back(std::make_shared<PolyCircle>(centerX_-(scaleX_/2.5), centerY_+(scaleY_/1.8), angle_, scaleX_/28.f, scaleY_/28.f, 1.f, 1.f, 1.f));
     // right eye
     _myShapes.push_back(std::make_shared<PolyCircle>(centerX_+(scaleX_/2.4), centerY_+(scaleY_/1.9), angle_, scaleX_/8.f, scaleY_/8.f, 0.f, 0.f, 0.f));
     _myShapes.push_back(std::make_shared<PolyCircle>(centerX_+(scaleX_/2.3), centerY_+(scaleY_/1.8), angle_, scaleX_/28.f, scaleY_/28.f, 1.f, 1.f, 1.f));
-    
     // nose
     _myShapes.push_back(std::make_shared<PolyTriangle>(centerX_, centerY_+(scaleY_/4.f), angle_+180.f, scaleX_/40.f, scaleY_/20.f, 1.f, 1.f, 1.f));
     std::cout << "Animal was created at " << centerX_ << ", " << centerY_ << std::endl;
