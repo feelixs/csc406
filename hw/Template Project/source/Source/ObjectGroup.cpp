dd2b7f867eded175e5ffd74547d0932d45ddaa69
Michael Felix <mikemh@uri.edu>
2023-10-10 23:13:18 -0400

    start objectgropu

---
+++ hw/Template Project/source/Source/ObjectGroup.cpp
@@ -0,0 +1,23 @@
+//
+//  ObjectGroup.cpp
+//  hw2
+//
+//  Created by Michael Felix on 10/10/23.
+//
+
+#include "ObjectGroup.hpp"
+
+
+ObjectGroup::ObjectGroup(GroupType type, GroupSize size, int num, float x, float y):
+    GraphicObject(x, y, 0),
+    type_(type),
+    size_(size),
+    num_(num)
+{
+    
+}
+
+
+void ObjectGroup::draw() const {
+    
+}
