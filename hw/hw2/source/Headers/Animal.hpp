d38d1e67622e91ccaa2c46a11f2f532a0e5b1c8b
Michael Felix <mikemh@uri.edu>
2023-10-13 10:48:17 -0400

    new draw approach works

--- hw/hw2/source/Headers/Animal.hpp
+++ hw/hw2/source/Headers/Animal.hpp
@@ -23,15 +23,19 @@ class Animal: public GraphicObject
 
         static std::vector<std::shared_ptr<PolyShape>> _myShapes;
     
-        // shape points of circle calculated in initCircle()
         static float** _circlePoints; // Nx2 float
         static float** _loadedShapePnts; // Nx2 float
+        static const int _numCirPoints;
+        static float** _trianPoints;
     
+        GLuint circleVertices_;
+        GLuint triangleVertices_;
     public:
         Animal(float centerX, float centerY, float angle, float scaleX, float scaleY, float r, float g, float b);
         Animal(Point centerPoint, float angle, float scale, float r, float g, float b);
         ~Animal() = default;
         
+        void drawRightWing(float atx, float aty, float atangle, float r, float g, float b, float scale) const;
         void draw() const;
         void update(float dx);
         void setColor(float r, float g, float b);
