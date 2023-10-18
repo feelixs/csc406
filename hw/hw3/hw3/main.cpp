5760929f13b8e84b774f38a4713aec814e3b705d
Michael Felix <mikemh@uri.edu>
2023-10-18 09:08:16 -0400

    start proj setup

---
+++ hw/hw3/hw3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw3
+//
+//  Created by Michael Felix on 10/18/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
