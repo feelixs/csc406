7b4a752b350181c1e6e01b3618ca14dcbf495bdc
Michael Felix <mikemh@uri.edu>
2023-10-25 10:22:43 -0400

    use namespace in asteroid

--- hw/hw3/hw3/Asteroid.hpp
+++ hw/hw3/hw3/Asteroid.hpp
@@ -13,26 +13,26 @@
 #include "GraphicObject.h"
 #include "AnimatedObject.h"
 
-namespace earshooter {
-    class Asteroid: public GraphicObject, public AnimatedObject {
-    private:
-        float scaleX_, scaleY_;
-        
-    public:
-        Asteroid(float centerX, float centerY, float angle, float width, float height,
-                float vx, float vy);
-        Asteroid(const WorldPoint& pt, float angle, float spin, float width, float height, const Velocity& vel);
-        
-        void draw() const;
-        bool isInside(const WorldPoint& pt);
-        
-        inline float getScaleY() {
-            return scaleY_;
-        }
-        inline float getScaleX() {
-            return scaleX_;
-        }
-    };
-}
+using namespace earshooter;
+
+class Asteroid: public GraphicObject, public AnimatedObject {
+private:
+    float scaleX_, scaleY_;
+    
+public:
+    Asteroid(float centerX, float centerY, float angle, float width, float height,
+            float vx, float vy);
+    Asteroid(const WorldPoint& pt, float angle, float spin, float width, float height, const Velocity& vel);
+    
+    void draw() const;
+    bool isInside(const WorldPoint& pt);
+    
+    inline float getScaleY() {
+        return scaleY_;
+    }
+    inline float getScaleX() {
+        return scaleX_;
+    }
+};
 
 #endif /* Asteroid_hpp */
