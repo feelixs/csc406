6c012a1a9d9abf6f57b596d8d64e2ec317e73088
Michael Felix <mikemh@uri.edu>
2023-10-26 15:40:24 -0400

    Update Spaceship.cpp

--- hw/hw3/hw3/Spaceship.cpp
+++ hw/hw3/hw3/Spaceship.cpp
@@ -29,7 +29,7 @@ void Spaceship::draw() const {
     glTranslatef(getX(), getY(), 0.f);
     
     // apply rotation
-    glRotatef(getAngle() - 90, 0.f, 0.f, 1.f);
+    glRotatef(getAngle() - 90, 0.f, 0.f, 1.f); // the spaceship model is drawn wrong by 90 degrees
     
     //    apply the radius as a scale
     glScalef(1.f, 1.f, 1.f);
