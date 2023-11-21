86442102dfb2737565f6de85e9494daac0495dba
Michael Felix <mikemh@uri.edu>
2023-11-21 08:49:09 -0500

    create quadmesh3d

---
+++ hw/hw4/hw4/hw4/Headers/QuadMesh3D.h
@@ -0,0 +1,12 @@
+//
+//  QuadMesh3D.hpp
+//  hw4
+//
+//  Created by Michael Felix on 11/21/23.
+//
+
+#ifndef QuadMesh3D_h
+#define QuadMesh3D_h
+
+
+#endif /* QuadMesh3D_h */
---
+++ hw/hw4/hw4/hw4/Source/QuadMesh3D.cpp
@@ -0,0 +1,8 @@
+//
+//  QuadMesh3D.cpp
+//  hw4
+//
+//  Created by Michael Felix on 11/21/23.
+//
+
+#include "QuadMesh3D.h"
