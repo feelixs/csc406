21a5f3cd00379c6e87422d4540f01c0a83603afb
Michael Felix <mikemh@uri.edu>
2023-11-21 08:47:38 -0500

    hw4 init

---
+++ hw/hw4/hw4/hw4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw4
+//
+//  Created by Michael Felix on 11/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
