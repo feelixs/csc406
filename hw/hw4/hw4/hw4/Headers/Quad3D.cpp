3bedcef33dcc9e31c68eb48260627eeeba3a75a9
Michael Felix <mikemh@uri.edu>
2023-11-21 08:49:58 -0500

    quad3d

---
+++ hw/hw4/hw4/hw4/Headers/Quad3D.cpp
@@ -0,0 +1,8 @@
+//
+//  Quad3D.cpp
+//  hw4
+//
+//  Created by Michael Felix on 11/21/23.
+//
+
+#include "Quad3D.h"
---
+++ hw/hw4/hw4/hw4/Headers/Quad3D.h
@@ -0,0 +1,12 @@
+//
+//  Quad3D.hpp
+//  hw4
+//
+//  Created by Michael Felix on 11/21/23.
+//
+
+#ifndef Quad3D_h
+#define Quad3D_h
+
+
+#endif /* Quad3D_h */
